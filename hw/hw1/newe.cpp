239e5fb309252a24e8c26b39853cb134b2e57098
nonoivan1234 <nonoivan0627@gmail.com>
2024-02-29 14:25:13 +0800

    pE AC

--- hw/hw1/newe.cpp
+++ hw/hw1/newe.cpp
@@ -1,12 +1,5 @@
 #pragma comment(linker, "/stack:200000000")
 #pragma GCC optimize("Ofast")
-#pragma GCC optimize ("unroll-loops")
-#pragma GCC optimize ("-ffast-math")
-#pragma GCC optimize ("no-stack-protector")
-#pragma GCC optimize ("inline")
-#pragma GCC optimize ("omit-frame-pointer")
-#pragma GCC optimize ("unsafe-math-optimizations")
-#pragma GCC optimize ("unsafe-loop-optimizations")
 #pragma GCC target("avx,avx2,fma,sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx")
 #include <iostream>
 #include <algorithm>
@@ -21,8 +14,33 @@ static const int orig[24] = {0, 0, 0, 0,
     1, 1, 2, 2, 3, 3, 4, 4,
     5, 5, 5, 5
 };
-static const __uint128_t MASK = (1 << BITS_PER_COLOR) - 1;
-__uint128_t shiftedMasks[NUM_FACELETS];
+
+struct Facelet{
+    unsigned int color0 : BITS_PER_COLOR;
+    unsigned int color1 : BITS_PER_COLOR;
+    unsigned int color2 : BITS_PER_COLOR;
+    unsigned int color3 : BITS_PER_COLOR;
+    unsigned int color4 : BITS_PER_COLOR;
+    unsigned int color5 : BITS_PER_COLOR;
+    unsigned int color6 : BITS_PER_COLOR;
+    unsigned int color7 : BITS_PER_COLOR;
+    unsigned int color8 : BITS_PER_COLOR;
+    unsigned int color9 : BITS_PER_COLOR;
+    unsigned int color10 : BITS_PER_COLOR;
+    unsigned int color11 : BITS_PER_COLOR;
+    unsigned int color12 : BITS_PER_COLOR;
+    unsigned int color13 : BITS_PER_COLOR;
+    unsigned int color14 : BITS_PER_COLOR;
+    unsigned int color15 : BITS_PER_COLOR;
+    unsigned int color16 : BITS_PER_COLOR;
+    unsigned int color17 : BITS_PER_COLOR;
+    unsigned int color18 : BITS_PER_COLOR;
+    unsigned int color19 : BITS_PER_COLOR;
+    unsigned int color20 : BITS_PER_COLOR;
+    unsigned int color21 : BITS_PER_COLOR;
+    unsigned int color22 : BITS_PER_COLOR;
+    unsigned int color23 : BITS_PER_COLOR;
+};
 
 class PocketCube 
 {
@@ -35,19 +53,66 @@ public:
     void SetFacelet(int index, __uint128_t color);
     int GetFacelet(int index) const;
 private:
-    __uint128_t Cube = 0;
-
+    Facelet Cube;
 };
 
 void PocketCube::SetFacelet(int index, __uint128_t color) {
-    Cube &= ~(shiftedMasks[index]);
-    Cube |= (color << (index * BITS_PER_COLOR));
+    switch(index){
+        case 0: Cube.color0 = color; break;
+        case 1: Cube.color1 = color; break;
+        case 2: Cube.color2 = color; break;
+        case 3: Cube.color3 = color; break;
+        case 4: Cube.color4 = color; break;
+        case 5: Cube.color5 = color; break;
+        case 6: Cube.color6 = color; break;
+        case 7: Cube.color7 = color; break;
+        case 8: Cube.color8 = color; break;
+        case 9: Cube.color9 = color; break;
+        case 10: Cube.color10 = color; break;
+        case 11: Cube.color11 = color; break;
+        case 12: Cube.color12 = color; break;
+        case 13: Cube.color13 = color; break;
+        case 14: Cube.color14 = color; break;
+        case 15: Cube.color15 = color; break;
+        case 16: Cube.color16 = color; break;
+        case 17: Cube.color17 = color; break;
+        case 18: Cube.color18 = color; break;
+        case 19: Cube.color19 = color; break;
+        case 20: Cube.color20 = color; break;
+        case 21: Cube.color21 = color; break;
+        case 22: Cube.color22 = color; break;
+        case 23: Cube.color23 = color; break;
+    }
 }
 
 int PocketCube::GetFacelet(int index) const {
-    int shiftedValue = Cube >> (index * BITS_PER_COLOR);
-    int maskedValue = shiftedValue & MASK;
-    return maskedValue;
+    switch(index){
+        case 0: return Cube.color0;
+        case 1: return Cube.color1;
+        case 2: return Cube.color2;
+        case 3: return Cube.color3;
+        case 4: return Cube.color4;
+        case 5: return Cube.color5;
+        case 6: return Cube.color6;
+        case 7: return Cube.color7;
+        case 8: return Cube.color8;
+        case 9: return Cube.color9;
+        case 10: return Cube.color10;
+        case 11: return Cube.color11;
+        case 12: return Cube.color12;
+        case 13: return Cube.color13;
+        case 14: return Cube.color14;
+        case 15: return Cube.color15;
+        case 16: return Cube.color16;
+        case 17: return Cube.color17;
+        case 18: return Cube.color18;
+        case 19: return Cube.color19;
+        case 20: return Cube.color20;
+        case 21: return Cube.color21;
+        case 22: return Cube.color22;
+        case 23: return Cube.color23;
+    }
+    return 0;
 }
 
 
@@ -67,12 +132,6 @@ std::ostream& operator<<(std::ostream& os, const PocketCube& pc) {
 PocketCube::PocketCube() {
     for(int i = 0; i < NUM_FACELETS; i++){
         SetFacelet(i, orig[i]);
-    }
-
-    if(shiftedMasks[0] == 0){
-        for(int i = 0; i < NUM_FACELETS; i++){
-            shiftedMasks[i] = (MASK << (i * BITS_PER_COLOR));
-        }
     }
 }
 
@@ -178,6 +237,8 @@ void Test1() {
 
 void Test2() { 
     PocketCube a;
+    std::cout << a << std::endl;
+    std::cout << sizeof(a) << std::endl;
  }
 void Test3() { /* HIDDEN */ }
 void Test4() { /* HIDDEN */ }
